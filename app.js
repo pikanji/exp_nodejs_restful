217e6f89d478fce4f441e8515e440552a931fe4b
Kanji Furuhashi <kanji.furuhashi@gree.co.jp>
2012-05-25 09:21:47 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+//app.get('/', routes.index);
+
+app.get('/', function(req, res){
+  console.log(req.query); // for logging
+
+  var name = "";
+  // NAMEパラメタが空でなければ画面に表示
+  if (req.query.name) {
+    name = req.query.name;
+  }
+
+  // Renders from template
+  //res.render('get.ejs', { locals: { name: name } });
+  res.json(['ok']);
+});
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
